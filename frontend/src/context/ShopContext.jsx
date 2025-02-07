0200a84015f8a82ea4d3bdea3d45e0fc50263368
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-02-07 12:52:43 +0530

    serach feature

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import {products} from "../assets/assets"
 
 
@@ -9,11 +9,17 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
+    const [search, setSearch] = useState('')
+    const [showSearch, setShowSearch] = useState(false)
 
     const value = {
         products,
         currency,
-        delivery_fee
+        delivery_fee,
+        search,
+        setSearch,
+        showSearch,
+        setShowSearch
     }
 
     return (
